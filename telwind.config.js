c0bff379c8c942295c537cca14649d4a217a3c49
dev-nahianrafi <iamalnahiyan@gmail.com>
2023-12-24 02:08:40 +0600

    3 section done

---
+++ telwind.config.js
@@ -0,0 +1,20 @@
+tailwind.config = {
+    theme: {
+      extend: {
+        colors: {
+          clifford: '#da373d',
+          headerbg: '#282828',
+          red: '#F40404',
+        },
+        maxWidth: {
+          'container': '1144px',
+        },
+        fontFamily: {
+          'poppins': "'Poppins', sans-serif",
+        },
+        backgroundImage: {
+          'banner': "linear-gradient(rgba(0, 0, 0, 0.5),rgba(0, 0, 0, 0.5)) ,url('images/banner.jpg')",
+        }
+      }
+    }
+  }
